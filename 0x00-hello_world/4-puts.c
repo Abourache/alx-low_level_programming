7308089f534711c239b130d33a991bdf68726c69
Abourache <abouracheibrahime17@gmail.com>
2023-06-15 18:28:00 -0700

    fix puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  *main - entry print
- *return: always 0 (Success)
+ *
+ *return: always 0
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
