579147a46d025241515004673b72491a3e339b84
Abourache <abouracheibrahime17@gmail.com>
2023-06-26 13:01:47 -0700

    function that prints n elements of an array of integers, followed by a new line

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int num, n, i;
+
+	num = 0;
+
+	while (str[num] != '\0')
+	{
+		num++;
+	}
+
+	if (num % 2 == 0)
+	{
+		for (i = num / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (num % 2)
+	{
+		for (n = (num - 1) / 2; n < num - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
