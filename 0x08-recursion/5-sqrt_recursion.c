3bc6d9f6854e2c196106def38e0f9ec7c4d3a32c
Abourache <abouracheibrahime17@gmail.com>
2023-07-05 15:21:35 -0700

    returns 1 if the strings can be considered identica

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * sqrt2 - Makes possible to evaluate from 1 to n
+ * @x: same number as n
+ * @y: number that iterates from 1 to n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int sqrt2(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+	else if (y * y > x)
+		return (-1);
+	return (sqrt2(x, y + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
